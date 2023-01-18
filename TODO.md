854ae5675ee5bc9df61bfa415a6e2cf0cdd3a433
Jesper Beisner <jesper.beisner+github@gmail.com>
2023-01-18 20:53:05 +0100

    Active tab choice will now be saved in local storage

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODOs
+
+- Pagination and search on index page
+- Generate race change/name change pictures as well
+- i18n
+- Build usable prod docker image and autobuild in github action
+- Change database access logs
+- ~~Weltauswahl in LocalStorage speichern~~
+- Track xp gains for a single day
--- public/js/script.js
+++ public/js/script.js
@@ -11,38 +11,51 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    const actionFreewarTab = document.getElementById('action-freewar-tab');
-    const chaosFreewarTab = document.getElementById('chaos-freewar-tab');
-    const actionFreewarTabContent = document.getElementById('action-freewar-tab-content');
-    const chaosFreewarTabContent = document.getElementById('chaos-freewar-tab-content');
-
-    if (
-      actionFreewarTab !== null
-      && chaosFreewarTab !== null
-      && actionFreewarTabContent !== null
-      && chaosFreewarTabContent !== null
-    ) {
-        actionFreewarTab.addEventListener('click', () => {
-            actionFreewarTab.classList.add('is-active');
-            chaosFreewarTab.classList.remove('is-active');
-
-            actionFreewarTabContent.classList.remove('is-hidden');
-            chaosFreewarTabContent.classList.add('is-hidden');
-        });
-
-        chaosFreewarTab.addEventListener('click', () => {
-            chaosFreewarTab.classList.add('is-active');
-            actionFreewarTab.classList.remove('is-active');
-
-            chaosFreewarTabContent.classList.remove('is-hidden');
-            actionFreewarTabContent.classList.add('is-hidden');
-        });
-    }
-
     const $infoTexts = document.querySelectorAll('.info-text');
+
     $infoTexts.forEach((infoText) => {
         setTimeout(() => {
             infoText.classList.add('is-hidden');
         }, 2500);
     });
+
+    const activeTab = window.localStorage.getItem('active-tab');
+    const actionFreewarTab = document.getElementById('action-freewar-tab');
+    const chaosFreewarTab = document.getElementById('chaos-freewar-tab');
+    const actionFreewarTabContent = document.getElementById('action-freewar-tab-content');
+    const chaosFreewarTabContent = document.getElementById('chaos-freewar-tab-content');
+
+    const activateActionFreewarTab = () => {
+        actionFreewarTab.classList.add('is-active');
+        chaosFreewarTab.classList.remove('is-active');
+        actionFreewarTabContent.classList.remove('is-hidden');
+        chaosFreewarTabContent.classList.add('is-hidden');
+    }
+
+    const activateChaosFreewarTab = () => {
+        chaosFreewarTab.classList.add('is-active');
+        actionFreewarTab.classList.remove('is-active');
+        chaosFreewarTabContent.classList.remove('is-hidden');
+        actionFreewarTabContent.classList.add('is-hidden');
+    }
+
+    if (activeTab !== null && actionFreewarTab !== null && chaosFreewarTab !== null && actionFreewarTabContent !== null && chaosFreewarTabContent !== null) {
+        if (activeTab === 'action-freewar') {
+            activateActionFreewarTab();
+        }
+
+        if (activeTab === 'chaos-freewar') {
+            activateChaosFreewarTab();
+        }
+
+        actionFreewarTab.addEventListener('click', () => {
+            activateActionFreewarTab();
+            window.localStorage.setItem('active-tab', 'action-freewar');
+        });
+
+        chaosFreewarTab.addEventListener('click', () => {
+            activateChaosFreewarTab();
+            window.localStorage.setItem('active-tab', 'chaos-freewar');
+        });
+    }
 });
