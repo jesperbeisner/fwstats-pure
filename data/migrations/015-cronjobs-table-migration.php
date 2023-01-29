43f9f9770c4a63dbbab6d812467383ef0d79b99a
Jesper Beisner <jesper.beisner+github@gmail.com>
2023-01-29 11:48:24 +0100

    Add index to cronjobs created

--- data/migrations/015-cronjobs-table-migration.php
+++ data/migrations/015-cronjobs-table-migration.php
@@ -3,5 +3,6 @@
 declare(strict_types=1);
 
 return [
-    "CREATE TABLE IF NOT EXISTS cronjobs (id INTEGER PRIMARY KEY, created DATETIME NOT NULL);"
+    "CREATE TABLE IF NOT EXISTS cronjobs (id INTEGER PRIMARY KEY, created DATETIME NOT NULL)",
+    "CREATE INDEX IF NOT EXISTS cronjobs_created_index ON cronjobs(created)",
 ];
